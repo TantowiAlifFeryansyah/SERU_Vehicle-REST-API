b01bc1b17bd5a38ca317f22609e4ba03c34c47b7
Tantowi Alif Feryansyah <tantowialif15@gmail.com>
2023-06-06 04:08:28 +0700

    donee

--- controller/index.js
+++ controller/index.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
 
 class Controller {
+
+    // USER ==========================================================>
     static async register(req, res, next) {
         try {
             const { name, password, is_admin } = req.body
@@ -90,6 +92,357 @@ class Controller {
             const { id } = req.params
             const data = await User.destroy({ where: { id } })
             res.status(200).json({ message: 'User deleted successfully.', data })
+        } catch (error) {
+            console.error('error deleting:', error);
+            next(error)
+        }
+    }
+
+    // Vehicle ==========================================================>
+    // Vehicle Brands
+    static async createBrands(req, res, next) {
+        try {
+            const { name } = req.body
+            const data = await Vehicle_brand.create({ name });
+            res.status(201).json({ message: 'vehicle_brandssuccessfully created', data })
+        } catch (error) {
+            console.error('error creating:', error);
+            next(error)
+        }
+    }
+
+    static async getBrands(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            const offset = (page - 1) * limit
+
+            const total = await Vehicle_brand.count()
+            const pages = Math.ceil(total / limit)
+
+            const data = await Vehicle_brand.findAll()
+            res.status(200).json({ message: 'Get Data', data, total, limit, offset, pages, page })
+
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async getBrandId(req, res, next) {
+        const userId = req.params.id;
+        try {
+            const data = await Vehicle_brand.findByPk(userId)
+            if (data) {
+                res.status(200).json({ message: 'Get Data', data })
+            } else {
+                res.status(200).json({ message: 'User not found', data })
+            }
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async updateBrands(req, res, next) {
+        try {
+            const { id } = req.params
+            const { name } = req.body
+            const data = await Vehicle_brand.update({ name }, {
+                where: { id }
+            })
+            res.status(200).json({ message: 'Uppdating successfully.', data })
+        } catch (error) {
+            console.error('error upddating:', error);
+            next(error)
+        }
+    }
+
+    static async deleteBrands(req, res, next) {
+        try {
+            const { id } = req.params
+            const data = await Vehicle_brand.destroy({ where: { id } })
+            res.status(200).json({ message: 'Deleted successfully.', data })
+        } catch (error) {
+            console.error('error deleting:', error);
+            next(error)
+        }
+    }
+
+    // Vehicle Types
+    static async createTypes(req, res, next) {
+        try {
+            const { name, brand_id } = req.body
+            const data = await Vehicle_types.create({ name, brand_id });
+            res.status(201).json({ message: 'vehicle_brandssuccessfully created', data })
+        } catch (error) {
+            console.error('error creating:', error);
+            next(error)
+        }
+    }
+
+    static async getTypes(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            const offset = (page - 1) * limit
+
+            const total = await Vehicle_types.count()
+            const pages = Math.ceil(total / limit)
+
+            const data = await Vehicle_types.findAll()
+            res.status(200).json({ message: 'Get Data', data, total, limit, offset, pages, page })
+
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async getTypesId(req, res, next) {
+        const userId = req.params.id;
+        try {
+            const data = await Vehicle_types.findByPk(userId)
+            if (data) {
+                res.status(200).json({ message: 'Get Data', data })
+            } else {
+                res.status(200).json({ message: 'User not found', data })
+            }
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async updateTypes(req, res, next) {
+        try {
+            const { id } = req.params
+            const { name, brand_id } = req.body
+            const data = await Vehicle_types.update({ name, brand_id }, {
+                where: { id }
+            })
+            res.status(200).json({ message: 'Uppdating successfully.', data })
+        } catch (error) {
+            console.error('error upddating:', error);
+            next(error)
+        }
+    }
+
+    static async deleteTypes(req, res, next) {
+        try {
+            const { id } = req.params
+            const data = await Vehicle_types.destroy({ where: { id } })
+            res.status(200).json({ message: 'Deleted successfully.', data })
+        } catch (error) {
+            console.error('error deleting:', error);
+            next(error)
+        }
+    }
+
+    // Vehicle Models
+    static async createModels(req, res, next) {
+        try {
+            const { name, type_id } = req.body
+            const data = await Vehicle_models.create({ name, type_id });
+            res.status(201).json({ message: 'vehicle successfully created', data })
+        } catch (error) {
+            console.error('error creating:', error);
+            next(error)
+        }
+    }
+
+    static async getModels(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            const offset = (page - 1) * limit
+
+            const total = await Vehicle_models.count()
+            const pages = Math.ceil(total / limit)
+
+            const data = await Vehicle_models.findAll()
+            res.status(200).json({ message: 'Get Data', data, total, limit, offset, pages, page })
+
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async getModelsId(req, res, next) {
+        const userId = req.params.id;
+        try {
+            const data = await Vehicle_models.findByPk(userId)
+            if (data) {
+                res.status(200).json({ message: 'Get Data', data })
+            } else {
+                res.status(200).json({ message: 'User not found', data })
+            }
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async updateModels(req, res, next) {
+        try {
+            const { id } = req.params
+            const { name, type_id } = req.body
+            const data = await Vehicle_models.update({ name, type_id }, {
+                where: { id }
+            })
+            res.status(200).json({ message: 'Uppdating successfully.', data })
+        } catch (error) {
+            console.error('error upddating:', error);
+            next(error)
+        }
+    }
+
+    static async deleteModels(req, res, next) {
+        try {
+            const { id } = req.params
+            const data = await Vehicle_models.destroy({ where: { id } })
+            res.status(200).json({ message: 'Deleted successfully.', data })
+        } catch (error) {
+            console.error('error deleting:', error);
+            next(error)
+        }
+    }
+
+    // Vehicle Years
+    static async createYears(req, res, next) {
+        try {
+            const { year } = req.body
+            const data = await Vehicle_years.create({ year });
+            res.status(201).json({ message: 'vehicle successfully created', data })
+        } catch (error) {
+            console.error('error creating:', error);
+            next(error)
+        }
+    }
+
+    static async getYears(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            const offset = (page - 1) * limit
+
+            const total = await Vehicle_years.count()
+            const pages = Math.ceil(total / limit)
+
+            const data = await Vehicle_years.findAll()
+            res.status(200).json({ message: 'Get Data', data, total, limit, offset, pages, page })
+
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async getYearsId(req, res, next) {
+        const userId = req.params.id;
+        try {
+            const data = await Vehicle_years.findByPk(userId)
+            if (data) {
+                res.status(200).json({ message: 'Get Data', data })
+            } else {
+                res.status(200).json({ message: 'User not found', data })
+            }
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async updateYears(req, res, next) {
+        try {
+            const { id } = req.params
+            const { year } = req.body
+            const data = await Vehicle_years.update({ year }, {
+                where: { id }
+            })
+            res.status(200).json({ message: 'Uppdating successfully.', data })
+        } catch (error) {
+            console.error('error upddating:', error);
+            next(error)
+        }
+    }
+
+    static async deleteYears(req, res, next) {
+        try {
+            const { id } = req.params
+            const data = await Vehicle_years.destroy({ where: { id } })
+            res.status(200).json({ message: 'Deleted successfully.', data })
+        } catch (error) {
+            console.error('error deleting:', error);
+            next(error)
+        }
+    }
+
+    // Vehicle Years
+    static async createPricelist(req, res, next) {
+        try {
+            const { year_id, model_id } = req.body
+            const data = await Pricelist.create({ year_id, model_id });
+            res.status(201).json({ message: 'vehicle successfully created', data })
+        } catch (error) {
+            console.error('error creating:', error);
+            next(error)
+        }
+    }
+
+    static async getPricelist(req, res, next) {
+        try {
+            const page = parseInt(req.query.page) || 1
+            const limit = 5
+            const offset = (page - 1) * limit
+
+            const total = await Pricelist.count()
+            const pages = Math.ceil(total / limit)
+
+            const data = await Pricelist.findAll()
+            res.status(200).json({ message: 'Get Data', data, total, limit, offset, pages, page })
+
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async getPricelistId(req, res, next) {
+        const userId = req.params.id;
+        try {
+            const data = await Pricelist.findByPk(userId)
+            if (data) {
+                res.status(200).json({ message: 'Get Data', data })
+            } else {
+                res.status(200).json({ message: 'User not found', data })
+            }
+        } catch (error) {
+            console.error('Internal server error:', error);
+            next(error)
+        }
+    }
+
+    static async updatePricelist(req, res, next) {
+        try {
+            const { id } = req.params
+            const { year } = req.body
+            const data = await Pricelist.update({ year }, {
+                where: { id }
+            })
+            res.status(200).json({ message: 'Uppdating successfully.', data })
+        } catch (error) {
+            console.error('error upddating:', error);
+            next(error)
+        }
+    }
+
+    static async deletePricelist(req, res, next) {
+        try {
+            const { id } = req.params
+            const data = await Pricelist.destroy({ where: { id } })
+            res.status(200).json({ message: 'Deleted successfully.', data })
         } catch (error) {
             console.error('error deleting:', error);
             next(error)
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,50 @@ var router = express.Router();
 const authentication = require('../middleware/auth')
 const controller = require('../controller')
 
-/* GET home page. */
+/* Create Vehicle */
+// ================================================
 
+// GET /vehicle
+router.get('/vehicle/brands', controller.getBrands);
+router.get('/vehicle/id', controller.getBrandId);
+
+router.get('/vehicle/types', controller.getTypes);
+router.get('/vehicle/id', controller.getTypesId);
+
+router.get('/vehicle', controller.getModels);
+router.get('/vehicle/id', controller.getModelsId);
+
+router.get('/vehicle/models', controller.getYearsId);
+router.get('/vehicle/id', controller.getYearsId);
+
+router.get('/vehicle/pricelist', controller.getPricelist);
+router.get('/vehicle/id', controller.getPricelistId);
+
+// POST /vehicle
+router.post('/vehicle/brands', controller.createBrands);
+router.post('/vehicle/types', controller.createTypes);
+router.post('/vehicle/models', controller.createModels);
+router.post('/vehicle/years', controller.createYears);
+router.post('/vehicle/pricelist', controller.createPricelist);
+
+// PATCH /vehicle/:id
+router.patch('/vehicle/:id', controller.updateBrands);
+router.patch('/vehicle/:id', controller.updateTypes);
+router.patch('/vehicle/:id', controller.updateModels);
+router.patch('/vehicle/:id', controller.updateYears);
+router.patch('/vehicle/:id', controller.updatePricelist);
+
+// DELETE /vehicle/:id
+router.delete('/vehicle/:id', controller.deleteBrands);
+router.delete('/vehicle/:id', controller.deleteTypes);
+router.delete('/vehicle/:id', controller.deleteModels);
+router.delete('/vehicle/:id', controller.deleteYears);
+router.delete('/vehicle/:id', controller.deletePricelist);
+
+// ================================================
+
+/* Create Users */
+// ================================================
 
 // GET /users
 router.get('/users', controller.getData);
@@ -12,18 +54,20 @@ router.get('/users', controller.getData);
 // GET /users/:id
 router.get('/users/:id', controller.getDataId);
 
-// GET /users/register
+// POST /users/register
 router.post('/users/register', controller.register);
 
-// GET /users/login || // POST /users
+// POST /users/login || // POST /users
 router.post('/users/login', controller.login);
 router.use(authentication)
 
 // PATCH /users/:id
 router.patch('/users/:id', controller.update);
 
-// / DELETE /users/:id
+// DELETE /users/:id
 router.delete('/users/:id', controller.delete);
+
+// ================================================
 
 module.exports = router;
 
